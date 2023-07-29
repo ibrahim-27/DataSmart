f876e9c6facaa232e6e3deb50d315e9364a38cdd
Muhammad Ibrahim <mibrahim.bscs20seecs@seecs.edu.pk>
2023-07-29 22:16:26 +0500

    spelling mistakes removed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -25,7 +25,7 @@ export default function Navbar() {
           {!nav?<AiOutlineClose size={24} color='white'></AiOutlineClose>:<AiOutlineMenu size={24} color='white'></AiOutlineMenu>}
         </div>
     <div className={!nav?'bg-[#000300] border-r border-r-gray-600 fixed top-0 left-0 w-3/4 h-full pt-4 px-8 ease-in-out duration-500':'fixed left-[-100%]'}>
-      <h1 className='text-3xl font-bold text-[#00df9a]'>REACT</h1>
+      <h1 className='text-3xl font-bold text-[#00df9a]'>DataSmart</h1>
       <ul className='text-white flex flex-col uppercase mt-8'>
           <li className='border-b border-b-gray-600 my-2 pb-2 hover:text-[#00df9a] cursor-pointer'>Home</li>
           <li className='border-b border-b-gray-600 my-2 pb-2 hover:text-[#00df9a] cursor-pointer'>Company</li>
